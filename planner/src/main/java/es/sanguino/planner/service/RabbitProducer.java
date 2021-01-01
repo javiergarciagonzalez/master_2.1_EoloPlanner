bae97b5b69fa1db1d15b1b6d8a93d454e1daf9bd
Miguel G. Sanguino <sanguino@me.com>
2021-01-01 14:18:20 +0100

    Producer sleep

--- planner/src/main/java/es/sanguino/planner/service/RabbitProducer.java
+++ planner/src/main/java/es/sanguino/planner/service/RabbitProducer.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RabbitProducer {
 
@@ -13,6 +15,13 @@ public class RabbitProducer {
 
 	public void sendMessage(Eoloplant eoloplant) {
 		eoloplant.advanceProgress();
+		if (eoloplant.getCompleted()) {
+			try {
+				Thread.sleep(1000 + new Random().nextInt(2000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		System.out.println(System.currentTimeMillis() + "  eoloplantCreationProgressNotifications: "+eoloplant.toString());
 		rabbitTemplate.convertAndSend("eoloplantCreationProgressNotifications", eoloplant);
 	}

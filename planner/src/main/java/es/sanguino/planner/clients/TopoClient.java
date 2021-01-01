3b8a8b72706159d46ed6c64c072f9b1113003843
Miguel G. Sanguino <sanguino@me.com>
2021-01-01 13:07:13 +0100

    TopoClient completed

--- planner/src/main/java/es/sanguino/planner/clients/TopoClient.java
+++ planner/src/main/java/es/sanguino/planner/clients/TopoClient.java
@@ -1,30 +1,20 @@
 package es.sanguino.planner.clients;
 
+import es.sanguino.planner.models.LandscapeResponse;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.Serializable;
 import java.util.concurrent.CompletableFuture;
 
 @Component
 public class TopoClient {
 
-    class LanscapeResponse implements Serializable {
-        public String id;
-        public String landscape;
-    }
-
     @Async
     public CompletableFuture<String> getLandscape(String city) {
-
-        System.out.println("topoooooo1111");
         RestTemplate restTemplate = new RestTemplate();
-        System.out.println("topoooooo2222 -> " + city);
         String url = "http://localhost:8080/api/topographicdetails/" + city;
-        System.out.println("topoooooo3333 -> " + url);
-        LanscapeResponse response = restTemplate.getForObject(url, LanscapeResponse.class);
-        System.out.println("topoooooo4444 -> " + response.landscape);
-        return CompletableFuture.completedFuture(response.landscape);
+        LandscapeResponse response = restTemplate.getForObject(url, LandscapeResponse.class);
+        return CompletableFuture.completedFuture(response.getLandscape());
     }
 }

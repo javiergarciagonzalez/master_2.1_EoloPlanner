afc99202de77a050d35eee7bf5361613109d4c75
Miguel G. Sanguino <sanguino@me.com>
2021-01-01 13:05:32 +0100

    Added LandscapeResponse model

---
+++ planner/src/main/java/es/sanguino/planner/models/LandscapeResponse.java
@@ -0,0 +1,32 @@
+package es.sanguino.planner.models;
+
+import java.io.Serializable;
+
+public class LandscapeResponse implements Serializable {
+    private String id;
+    private String landscape;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLandscape() {
+        return landscape;
+    }
+
+    public void setLandscape(String landscape) {
+        this.landscape = landscape;
+    }
+
+    @Override
+    public String toString() {
+        return "LandscapeResponse{" +
+                "id='" + id + '\'' +
+                ", landscape='" + landscape + '\'' +
+                '}';
+    }
+}

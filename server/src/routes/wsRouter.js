8556795b78bd70a968e68c1b234906968a9ea3fc
Miguel G. Sanguino <sanguino@me.com>
2021-01-03 16:46:45 +0100

    add debug to ws

--- server/src/routes/wsRouter.js
+++ server/src/routes/wsRouter.js
@@ -1,8 +1,9 @@
 import {saveClient} from '../models/EoloPlantsUsers.js'
+import DebugLib from 'debug';
+const debug = new DebugLib('ws');
 
 export default (ws, req) => {
+  debug('user connected', req.headers['sec-websocket-key']);
   saveClient(req.headers['sec-websocket-key'], ws);
-  console.log('user connected');
-  ws.send(JSON.stringify({'sec-websocket-key': req.headers['sec-websocket-key']}));
-
+  ws.send(JSON.stringify({'user-key': req.headers['sec-websocket-key']}));
 }

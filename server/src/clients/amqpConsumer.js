83b77a24713992d9e32f62ff75b92433cc728058
Miguel G. Sanguino <sanguino@me.com>
2021-01-03 16:48:24 +0100

    refactor consumer

--- server/src/clients/amqpConsumer.js
+++ server/src/clients/amqpConsumer.js
@@ -1,32 +1,26 @@
 import ch from '../connections/amqpConnection.js';
 import config from 'config';
 import {updateEoloPlant} from '../models/EoloPlant.js'
-import {getWs} from '../models/EoloPlantsUsers.js';
+import {getWs, getClients} from '../models/EoloPlantsUsers.js';
 import DebugLib from 'debug';
-const debug = new DebugLib('amqp:consumer');
 
+const debug = new DebugLib('amqp:consumer');
 const QUEUE = config.get('amqp.queues.progress');
 const OPTIONS = config.get('amqp.options');
-let clients;
 
-
-
-async function consumePlant (msg) {
-  const plant = JSON.parse(msg.content.toString());
-  debug('plant received', plant);
-  await updateEoloPlant(plant);
-  if (plant.progress === 100) {
-    clients.forEach(client => {
-      client.send(JSON.stringify(plant));
-    });
-  } else {
-    const ws = getWs(plant.id);
-    if (ws) ws.send(JSON.stringify(plant));
-  }
-}
-
-export default function amqpConsumer(wsEoloplants) {
-  clients = wsEoloplants.clients;
+export default function amqpConsumer() {
   ch.assertQueue(QUEUE, OPTIONS);
-  ch.consume(QUEUE, consumePlant, {noAck: true});
+  ch.consume(QUEUE, async msg => {
+    const plant = JSON.parse(msg.content.toString());
+    debug('plant received', plant);
+    await updateEoloPlant(plant);
+    if (plant.progress === 100) {
+      getClients().forEach(client => {
+        client.send(JSON.stringify(plant));
+      });
+    } else {
+      const ws = getWs(plant.id);
+      if (ws) ws.send(JSON.stringify(plant));
+    }
+  }, {noAck: true});
 }

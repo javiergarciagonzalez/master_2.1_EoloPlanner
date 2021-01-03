5c13bbabcf423c1866287aa44e2fdba9d0e37a63
Miguel G. Sanguino <sanguino@me.com>
2021-01-03 02:47:20 +0100

    feat: exec, on exit kill all subprocess

--- exec.js
+++ exec.js
@@ -1,20 +1,32 @@
-const { spawn } = require('child_process');
-function exec(serviceName, command){
+const {spawn} = require('child_process');
+
+function exec(serviceName, command) {
 
   console.log(`Started service [${serviceName}]`);
 
-  let cmd = spawn(command, [], { cwd: './' + serviceName, shell: true });
-  
-  cmd.stdout.on('data', function(data){
+  let cmd = spawn(command, [], {cwd: './' + serviceName, shell: true});
+
+  cmd.stdout.on('data', function (data) {
     process.stdout.write(`[${serviceName}] ${data}`);
   });
 
-  cmd.stderr.on('data', function(data){
+  cmd.stderr.on('data', function (data) {
     process.stderr.write(`[${serviceName}] ${data}`);
   });
+
+  return cmd;
 }
 
-exec('weatherservice', 'node src/server.js');
-exec('toposervice', 'mvn spring-boot:run');
-exec('server','node src/server.js');
-exec('planner','mvn spring-boot:run');
+const services = new Set();
+
+services.add(exec('weatherservice', 'node src/server.js'));
+services.add(exec('toposervice', 'mvn spring-boot:run'));
+services.add(exec('server', 'node src/server.js'));
+services.add(exec('planner', 'mvn spring-boot:run'));
+
+process.on('exit', async () => {
+  for (let service of services) {
+    service.stdin.pause();
+    service.kill();
+  }
+});

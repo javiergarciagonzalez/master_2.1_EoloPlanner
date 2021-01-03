e3e7cd7ae748bec98547b418b713b04aa1b5370c
Miguel G. Sanguino <sanguino@me.com>
2021-01-03 11:32:11 +0100

    complete CRUD to plants

--- server/src/routes/eoloplantsRouter.js
+++ server/src/routes/eoloplantsRouter.js
@@ -1,14 +1,16 @@
 import {Router} from 'express';
 import {EoloPlant} from '../models/EoloPlant.js'
-import amqpProducer from "../connections/amqpProducer.js";
+import amqpProducer from "../clients/amqpProducer.js";
 
 function getRoutes() {
   const routes = Router();
 
-  // routes.get('/api/eoloplants', async function (req, res, next) {
-  //     const books = await Book.find({}).select('_id title').exec();
-  //     return res.json(books);
-  // });
+  routes.get('/api/eoloplants', async function (req, res, next) {
+    const plants = await EoloPlant.findAll();
+    if (plants.length === 0)
+      return res.status(404).send('Not Found!');
+    return res.json(plants);
+  });
 
   routes.post('/api/eoloplants', async (req, res) => {
     const plant = EoloPlant.build(req.body)
@@ -22,8 +24,26 @@ function getRoutes() {
   });
 
   routes.get("/api/eoloplants/:id", async (req, res) => {
+    const plant = await EoloPlant.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (plant === null)
+      return res.status(404).send('Not Found!');
+    return res.json(plant);
+  });
 
-      return res.json(book);
+  routes.delete("/api/eoloplants/:id", async (req, res) => {
+    const plant = await EoloPlant.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (plant === null)
+      return res.status(404).send('Not Found!');
+    plant.destroy();
+    return res.json(plant);
   });
 
   return routes;

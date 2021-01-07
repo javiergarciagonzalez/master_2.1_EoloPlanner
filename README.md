dd696c156c8c3e03a58158c7663fdbe5e40def0a
Miguel G. Sanguino <sanguino@me.com>
2021-01-07 13:39:23 +0100

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# master_2.1_EoloPlanner
+# EoloPlanner
+
+## Iniciar MongoDB, MySQL y RabbitMQ
+```
+$ node docker.js
+```
+
+## Instalar dependencias
+```
+$ node install.js
+```
+
+## Ejecutar servicios
+```
+$ node exec.js
+```
--- docker.js
+++ docker.js
@@ -3,15 +3,15 @@ const { spawnSync } = require('child_process');
 function exec(serviceName, command){
 
   console.log(`Starting docker image for [${serviceName}]`);
-  console.log(`Folder: ${serviceName} Command: ${command}`);
+  console.log(`Command: ${command}`);
 
-  spawnSync(command, [], { 
-    cwd: serviceName,
+  spawnSync(command, [], {
+
     shell: true,
     stdio: 'inherit'
   });
 }
 
-exec('toposervice', 'docker run --rm -d -p 27017-27019:27017-27019 --name mongodb mongo');
-exec('server', 'docker run --rm -d -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=eoloplantsDB -p 3306:3306 --name mysql mysql:8.0.22');
-exec('planner', 'docker run --rm -d -p 5672:5672 -p 15672:15672 --name rabbitmq rabbitmq:3-management');
+exec('MongoDB', 'docker run --rm -d -p 27017-27019:27017-27019 --name mongodb mongo');
+exec('MySQL', 'docker run --rm -d -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=eoloplantsDB -p 3306:3306 --name mysql mysql:8.0.22');
+exec('RabbitMQ', 'docker run --rm -d -p 5672:5672 -p 15672:15672 --name rabbitmq rabbitmq:3-management');
